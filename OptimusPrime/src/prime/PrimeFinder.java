0e5aa9ec05935a44783febf7db011654e6381c28
Damien Hunter <a00014290@student.ait.ie>
2021-04-14 15:09:40 +0100

    Now with exception handling.

--- OptimusPrime/src/prime/PrimeFinder.java
+++ OptimusPrime/src/prime/PrimeFinder.java
@@ -14,13 +14,28 @@ public class PrimeFinder{
 		printList(primes);
 	}
 	
+	public static void findPrime(int lowerLimit, int upperLimit) throws LowerBoundsException{
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		
+		for(int x = lowerLimit; x < upperLimit; x++){
+			if(x < 1){
+				throw new LowerBoundsException();
+			}
+			if(x % 2 != 0 && x % 3 != 0 && x % 5 != 0 && x % 7 != 0){
+				primes.add(x);
+			}
+		}
+		printList(primes);
+	}
+	
 	public static void printList(ArrayList<Integer> primes){
 		for(int x = 0; x < primes.size(); x++){
 			System.out.println(primes.get(x));
 		}
 	}
 
-	public static void main(String[] args){
-		PrimeFinder.findPrime(10000);
+	public static void main(String[] args) throws LowerBoundsException{
+		//PrimeFinder.findPrime(10000);
+		PrimeFinder.findPrime(-1, 1000);
 	}
 }
